1251d600fc3ee647189397d4e0c043752acecc3f
Joaopedromach <estranhonerd123@gmail.com>
2023-07-28 22:43:31 -0300

    start

---
+++ src/main/kotlin/com/example/treino/TreinoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.treino
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TreinoApplication
+
+fun main(args: Array<String>) {
+	runApplication<TreinoApplication>(*args)
+}
